1b8e5d23f7dc40788195ef2da9abd89b78073a81
Daniel Vena <deaconvena+github@gmail.com>
2015-10-16 12:11:17 -0400

    initial commit

---
+++ src/base.js
@@ -0,0 +1,12 @@
+// @codekit-prepend 'iframeResizer.js';
+
+(function(window, undefined){
+	var iframe = window.document.querySelector('.entry-content iframe'),
+		isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
+	window.iFrameResize({
+		log: window._CMLS && window._CMLS.debug ? window._CMLS.debug : false,
+		checkOrigin: false,
+		heightCalculationMethod: isOldIE ? 'max' : 'lowestElement',
+		tolerance: 5
+	}, iframe);
+}(window));
