997dd7452946eda17b6a20ba5e96dd72e64bad33
Daniel Vena <deaconvena+github@gmail.com>
2015-10-16 12:28:01 -0400

    added id to iframe

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 // @codekit-prepend 'iframeResizer.js';
 
 (function(window, undefined){
-	var iframe = window.document.querySelector('.entry-content iframe'),
+	var iframe = window.document.querySelector('#botd-iframe'),
 		isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
 	window.iFrameResize({
 		log: window._CMLS && window._CMLS.debug ? window._CMLS.debug : false,
