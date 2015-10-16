d8f1226d598402649e38893f26cea2d75a4bc5c8
Daniel Vena <deaconvena+github@gmail.com>
2015-10-16 13:00:17 -0400

    attempting scroll to top when page changes

--- src/base.js
+++ src/base.js
@@ -1,12 +1,20 @@
 // @codekit-prepend 'iframeResizer.js';
 
 (function(window, undefined){
-	var iframe = window.document.querySelector('#botd-iframe'),
+	var iframe = window.document.querySelector('.botd-iframe'),
 		isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
 	window.iFrameResize({
 		log: window._CMLS && window._CMLS.debug ? window._CMLS.debug : false,
 		checkOrigin: false,
 		heightCalculationMethod: isOldIE ? 'max' : 'lowestElement',
-		tolerance: 5
+		tolerance: 5,
+		messageCallback: function(msg) {
+			var botd = jQuery('.botd-iframe');
+			if (msg == 'GO TO TOP') {
+				jQuery('html,body').animate({
+					scrollTop: botd.length && botd.offset() && botd.offset().top ? botd.offset().top : 0
+				}, 500);
+			}
+		}
 	}, iframe);
 }(window));
